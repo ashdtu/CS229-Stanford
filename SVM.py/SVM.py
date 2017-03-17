8cb1c133e005df1fa2b27ae2fa62ed98b4381633
Aasheesh Singh <aasheeshsingh_2k14@dtu.ac.in>
2017-03-17 16:57:42 +0530

    Initial commit

---
+++ SVM.py/SVM.py
@@ -0,0 +1,15 @@
+import sys
+print (sys.version)
+import numpy as np
+x_train=np.zeros(1000)
+print (x_train.shape)
+for i in range(x_train.shape[0]):
+    print x_train.shape[0]
+
+array=[[1,2,3], [4,5,6]]
+i=1
+array[1,2]={20}
+print array
+
+
+
